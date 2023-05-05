f32d81da696fe6f986e10e8038d9ea3371dcb61a
maggie <mjtice0@gmail.com>
2023-05-05 16:40:01 -0600

    Added string template to function and minimal css styling

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from addEventListener");
+  // console.log("Button clicked from addEventListener");
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        ` 
+  }
+  ulEl.innerHTML = listItems;
+}
