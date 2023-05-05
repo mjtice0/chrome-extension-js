f6ab25b7ebfcf9f002897204f3637394f5d909c8
maggie <mjtice0@gmail.com>
2023-05-05 15:31:29 -0600

    Removed onclick event and chaned into addEventListener function

---
+++ index.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked from addEventListener");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
