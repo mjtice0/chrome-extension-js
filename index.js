bc2aaafd59f1a5ee306b75ee634c70bf110f4841
maggie <mjtice0@gmail.com>
2023-05-07 18:24:02 -0600

    added logic to save tabs in save btn

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
-const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
 
 tabBtn.addEventListener("click", function () {
-  console.log(tabs[0].url);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {});
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
